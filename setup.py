a629e2dc69436d6efdf91397f76c591528315f37
Fabian van der Werf <fvanderwerf@gmail.com>
2015-12-18 17:38:24 +0100

    empty oneir-service project

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(
+        name='oneir-service',
+        version='0.1',
+        description='oneir-service',
+        author='Fabian van der Werf',
+        author_email='fvanderwerf@gmail.com',
+        py_modules=['oneir-service']
+        )
