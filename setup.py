ad26c48ae11da004c90748a5e60cc5c5913f1276
Fabian van der Werf <fvanderwerf@gmail.com>
2016-01-12 21:03:41 +0100

    set development version

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 
 from distutils.core import setup
 
+import subprocess
+
+git_commit_time = subprocess.check_output("git log -1 --format=%ci | awk '{ print $1 $2 }' | tr -cd [0-9]", shell=True) 
+
 setup(
         name='oneir-service',
-        version='0.1',
+        version='0.1.dev' + git_commit_time,
         description='oneir-service',
         author='Fabian van der Werf',
         author_email='fvanderwerf@gmail.com',
