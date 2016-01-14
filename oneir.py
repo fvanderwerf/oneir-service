ddad30fe59e0eda5581e689dcda84935fbdc3647
fabian <Fabian van der Werf>
2016-01-14 11:26:47 +0100

    send command to rabbitmq

--- oneir.py
+++ oneir.py
@@ -1,5 +1,20 @@
 
 from flask import Flask, request
+import pika
+
+
+# Create our connection object, passing in the on_open method
+
+credentials = pika.PlainCredentials('guest', 'guest')
+parameters = pika.ConnectionParameters(
+        '172.17.0.7'
+        5672,
+        '/',
+        credentials)
+
+pikaconn = pika.BlockingConnection(parameters=parameters)
+channel = pikaconn.channel()
+channel.queue_declare(queue='oneir')
 
 service = Flask("oneir-service")
 
@@ -10,10 +25,11 @@ def get_config():
 
 @service.route("/api/v1/oneir/command", methods=["POST"])
 def oneir_command():
-    if request.data == "off":
-        return "ok"
-    else:
-        return "nok"
+    json = request.get_json()
+    if "command" in json:
+        channel.basic_publish(exchange='', routing_key='oneir', body=json["command"])
+    
+    return ('', 204)
 
 if __name__ == "__main__":
     service.run(host='0.0.0.0', port=8080)
